3876b3646519eed171eed25b4cca13b05c164500
MaWo <ccmawo@gmail.com>
2015-09-25 01:06:14 +0200

    server.js renamed

--- ref/server.php
+++ ref/server.php
@@ -2,7 +2,7 @@
 error_log("original message:".$_POST['value'], 0);
 $message=base64_encode($_POST['value']);
 error_log("original message:".$message, 0);
-$a = shell_exec("nodejs serverb62f52d67a7fc1aa598dc32a9d9ea43d37cd0d9432e1ff7de8758444acccf632.js ".$message);
+$a = shell_exec("nodejs server.js ".$message);
 error_log("message to hash:".$a, 0);
 $a = str_replace("\n", '', $a);
 $a = str_replace("\r", '', $a);
@@ -20,7 +20,7 @@ $data = array(
 $data_string = '';
 foreach($data as $key=>$value) { $data_string .= $key.'='.$value.'&'; }
 rtrim($data_string, '&');
-$ch = curl_init('https://eu1.woll-e.net:8443/nhz');
+$ch = curl_init('https://hz-services.com:8443/nhz');
 curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux i686; rv:20.0) Gecko/20121230 Firefox/20.0');                                                                                                                                                                     
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                                                                                                                                                                                
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);                                                                                                                                                                                                                               
